14ce3e12d0f16318cdf3e5ecee91a589f6d3d2e1
0kkun <shinji.okumoto.01@gmail.com>
2022-04-11 12:46:07 +0900

    初めてのfunctionsを実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// 上でインポートしたfunctionsが持つメソッドの1つであるHTTPリクエストメソッドを利用
+// HTTPリクエストがくると関数が発火する
+export const helloWorld = functions.https.onRequest((request, response) => {
+    // FirebaseConsole（GUIの管理画面）のFunctionsにログを出力
+    functions.logger.info("Hello logs!", {structuredData: true});
+    // response.sendでリクエスト元に値を返す。今回は文字列を返している。
+    response.send("Hello from Firebase!");
+});
