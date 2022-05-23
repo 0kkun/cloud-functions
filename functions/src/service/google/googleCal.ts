95af523b430d06f3773770728cd0b1e9e520effe
0kkun <shinji.okumoto.01@gmail.com>
2022-05-24 08:53:04 +0900

    googleApiでトークン発行まで実装

---
+++ functions/src/service/google/googleCal.ts
@@ -0,0 +1,73 @@
+/* eslint-disable */
+import {Config, Credentials} from '../../types';
+import * as functions from 'firebase-functions';
+import {google} from 'googleapis';
+
+/**
+ * GoogleApi接続用のインスタンス生成
+ * 処理の流れ
+ * 1. クレデンシャル情報を環境変数から取得
+ * 2. クレデンシャルを使ってoAuth2Clientインスタンスを生成
+ * 3. 受け取ったコードを使ってアクセストークンとリフレッシュトークンを発行
+ */
+export default class GoogleCal {
+  /**
+   * googleへ接続する
+   * @return {Config} 一旦Configを返すようにする
+   */
+  connect() {
+    const credentials: Credentials = {
+      web: functions.config().googlecredentialsweb,
+    };
+    const config: Config = {
+      scopes: ['https://www.googleapis.com/auth/calendar.readonly'],
+      tokenPath: 'access-token.json',
+      auth: {},
+    };
+
+    return new Promise((resolve) => {
+      this.authorize(credentials, config).then((auth) => {
+        resolve(auth);
+      });
+    });
+  }
+
+  /**
+   * OAuth認証処理
+   * @param {Object} credentials 認証データ
+   * @param {Object} config 設定
+   * @return {Promise} object oAuth2Client.
+   */
+  authorize(credentials: Credentials, config: Config) {
+    const {client_secret, client_id, redirect_uris} = credentials.web;
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris);
+
+    console.log(config);
+
+    return new Promise((resolve) => {
+      this.getToken(oAuth2Client, '4/0AX4XfWguE7j0yI7JGGMSPKRy1R-Sue6QGLH_i9UehEVzRKjeoLtaeujTGtS9xdij1w4o5w').then(() => {
+        resolve(oAuth2Client);
+      })
+    });
+  }
+
+  /**
+   * OAuth2のアクセストークンとリフレッシュトークンを取得
+   * @param oAuth2Client {object} google.auth.OAuth2の戻り値
+   * @return (promise) object 有効なtokenを含めたgoogle.auth.OAuth2
+   */
+  getToken(oAuth2Client: any, code: string) {
+    return new Promise((resolve) => {
+        // 指定されたコードのアクセストークンを取得
+        oAuth2Client.getToken(code, (err: any, token: string) => {
+          if (err) {
+            functions.logger.error(err, {structuredData: true});
+            return;
+          }
+          functions.logger.info(token, {structuredData: true});
+          oAuth2Client.setCredentials(token);
+          resolve(oAuth2Client);
+        });
+    });
+  }
+}
